01977dbf0e2bb155c365b572d883890561539e18
EmmanuelSchaan <emmanuel.schaan@gmail.com>
2022-12-16 21:45:16 -0800

    added text, mult switches

--- indi-diy-goto/indi_diygotodriver.cpp
+++ indi-diy-goto/indi_diygotodriver.cpp
@@ -21,16 +21,17 @@ bool DIYGoToDriver::initProperties()
     // initialize the parent's properties first
     INDI::DefaultDevice::initProperties();
 
+    // Switch property
     SayHelloSP[HELLO_COMMAND].fill(
     "HELLO_COMMAND",// The name of the VALUE
     "Say Hello",    // The label of the VALUE
     ISS_OFF         // The switch state
     );
-    
+
     SayHelloSP.fill(
         getDeviceName(),  // The name of the device
         "SAY_HELLO",      // The name of the PROPERTY
-        "Hello Commands", // The label of the PROPERTY
+        "Example switch", // The label of the PROPERTY
         MAIN_CONTROL_TAB, // What tab should we be on?
         IP_RW,            // Let's make it read/write.
         ISR_ATMOST1,      // At most 1 can be on
@@ -46,6 +47,42 @@ bool DIYGoToDriver::initProperties()
     // device, then call this. It's up to you.
     defineProperty(&SayHelloSP);
 
+
+    // Text property
+    WhatToSayTP[0].fill("WHAT_TO_SAY", "What to say?", "Hello, world!");
+    WhatToSayTP.fill(
+          getDeviceName(), 
+          "WHAT_TO_SAY", 
+          "Example text", 
+          MAIN_CONTROL_TAB, 
+          IP_RW, 
+          60, 
+          IPS_IDLE);
+    defineProperty(&WhatToSayTP);
+
+    // Switch with multiple values
+    SayHiSP[SAY_HI_DEFAULT].fill(
+        "SAY_HI_DEFAULT",    // The name of the VALUE
+        "Say Hi",            // The label of the VALUE
+        ISS_OFF                 // The switch state
+        );
+    SayHiSP[SAY_HI_CUSTOM].fill(
+        "SAY_HI_CUSTOM",     // The name of the VALUE
+        "Say Custom",           // The label of the VALUE
+        ISS_OFF                 // The switch state
+        );
+    SayHiSP.fill(
+        getDeviceName(),  // The name of the device
+        "SAY_HI",      // The name of the PROPERTY
+        "Example mult switch", // The label of the PROPERTY
+        MAIN_CONTROL_TAB, // What tab should we be on?
+        IP_RW,            // Let's make it read/write.
+        ISR_ATMOST1,      // At most 1 can be on
+        60,               // With a timeout of 60 seconds
+        IPS_IDLE          // and an initial state of idle.
+    );        
+    defineProperty(&SayHiSP);
+
     return true;
 }
 
@@ -60,21 +97,72 @@ bool DIYGoToDriver::ISNewSwitch(const char *dev, const char *name, ISState *stat
         {
             // Log a message. This will show up in the control panel.
             LOG_INFO("Hello, world!");
-
             // Turn the switch back off
             SayHelloSP[HELLO_COMMAND].setState(ISS_OFF);
-
             // Set the property state back to idle
             SayHelloSP.setState(IPS_IDLE);
-
             // And actually inform INDI of those two operations
             SayHelloSP.apply();
-            
             // We're done!
+            return true;
+        }
+
+
+        if (SayHiSP.isNameMatch(name))
+        {
+            // Accept what we received.
+            SayHiSP.update(states, names, n);
+
+            // Find out what switch was clicked.
+            int index = SayHiSP.findOnSwitchIndex();
+            switch (index)
+            {
+            case SAY_HI_DEFAULT: // see how much better this is than direct indexes? USE AN ENUM!
+                LOG_INFO("Hi, world!");
+                break;
+            case SAY_HI_CUSTOM:
+                LOG_INFO(WhatToSayTP[0].getText());
+                break;
+            }
+            // Turn all switches back off.
+            SayHiSP.reset();
+            // Set the property state back to idle
+            SayHiSP.setState(IPS_IDLE);
+            // And actually inform INDI of those two operations
+            SayHiSP.apply();
+            return true;
+        }
+
+
+
+    }
+
+    // Nobody has claimed this, so let the parent handle it
+    return INDI::DefaultDevice::ISNewSwitch(dev, name, states, names, n);
+}
+
+
+// Function that gets called when new text is entered
+bool DIYGoToDriver::ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n)
+{
+    // Make sure it is for us.
+    if (dev != nullptr && strcmp(dev, getDeviceName()) == 0)
+    {
+        if (WhatToSayTP.isNameMatch(name))
+        {
+            // Update the property to what the client sent
+            // All elements in the property will now by synced with the client.
+            WhatToSayTP.update(texts, names, n);
+            // Set state to Idle
+            WhatToSayTP.setState(IPS_IDLE);
+            // Send back to client.
+            WhatToSayTP.apply();
+            //log the content of the property to the client
+            LOG_INFO(WhatToSayTP[0].getText());
             return true;
         }
     }
 
     // Nobody has claimed this, so let the parent handle it
-    return INDI::DefaultDevice::ISNewSwitch(dev, name, states, names, n);
+    return INDI::DefaultDevice::ISNewText(dev, name, texts, names, n);
 }
--- indi-diy-goto/indi_diygotodriver.h
+++ indi-diy-goto/indi_diygotodriver.h
@@ -2,6 +2,7 @@
 
 #include "libindi/defaultdevice.h"
 #include "indipropertyswitch.h"
+#include "indipropertytext.h"
 
 class DIYGoToDriver : public INDI::DefaultDevice
 {
@@ -18,7 +19,7 @@ public:
     virtual bool initProperties() override;
 
 
-// Example property: a simple switch
+// Example property: switch
 private:
     INDI::PropertySwitch SayHelloSP {1}; // A switch propety with 1 element.
     enum
@@ -26,11 +27,34 @@ private:
        HELLO_COMMAND
     };
 
+
+// Switch with multiple values
+// Use the inherent autoincrementing of an enum to generate our indexes.
+// This makes keeping track of multiple values on a property MUCH easier
+// than remembering indexes throughout your code.
+// The last value _N is used as the total count.
+enum
+{
+  SAY_HI_DEFAULT,
+  SAY_HI_CUSTOM,
+  SAY_HI_N,
+};
+INDI::PropertySwitch SayHiSP {SAY_HI_N};
+
+
+// Example property: text
+private:
+    INDI::PropertyText WhatToSayTP {1};
+
+
 // Function that gets called when a user clicks on a switch
 public:
     virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[],
                              int n) override;
 
+// Function that gets called when a user enters text
+public:
+    virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[],
+                           int n) override;
 };
-
 
